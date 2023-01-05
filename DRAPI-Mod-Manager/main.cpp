490b35163a4fc96258c493570b7ffdda29e30f37
DillyzThe1 <dyl1029an@gmail.com>
2023-01-05 17:23:03 -0600

    state enum and launcher data

--- DRAPI-Mod-Manager/main.cpp
+++ DRAPI-Mod-Manager/main.cpp
@@ -8,10 +8,55 @@ using namespace sf;
 VideoMode video(1280, 720);
 RenderWindow window(video, "DRAPI Mod Manager for Among Us");
 
+enum StateType {
+	Setup,    // when you're setting up your new among us directory 
+	Title,    // the title screen, showing everything cool
+	Main,     // the main menu, showing the buttons and options, alongside credits and information
+    Mods,     // the mods menu, where you find, install, & remove any mods you wish.
+	Progress  // the download progress screen, where you'll be until something finishes installing
+};
+
 Color bgcolor(15, 10, 25, 255);
+StateType curState = Title;
+
+// SETUP SCENE VARS
+// TITLE SCENE VARS
+// MAIN MENU SCENE VARS
+// MODS MENU SCENE VARS
+// PROGRESS SCENE SCENE VARS
+// :sadsping:
+
+long lastCpuTime = 0;
+void update(float secondsPassed) {
+	switch (curState) {
+		case Setup:
+			break;
+		case Title:
+			break;
+		case Main:
+			break;
+		case Mods:
+			break;
+		case Progress:
+			break;
+	}
+}
 
 void render() {
 	window.clear(bgcolor);
+
+	switch (curState) {
+		case Setup:
+			break;
+		case Title:
+			break;
+		case Main:
+			break;
+		case Mods:
+			break;
+		case Progress:
+			break;
+	}
 
 	window.display();
 }
@@ -29,6 +74,10 @@ int main() {
 	window.requestFocus();
 
 	while (window.isOpen()) {
+		long curTime = clock();
+		update((float)(curTime - lastCpuTime) / (float)1000);
+		lastCpuTime = curTime;
+
 		Event e;
 		while (window.pollEvent(e)) {
 			switch (e.type) {
