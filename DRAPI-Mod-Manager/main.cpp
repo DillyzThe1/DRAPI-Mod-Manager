35bbcff37ae2021a5acc19aabcd84123b08556f6
DillyzThe1 <dyl1029an@gmail.com>
2023-01-07 16:41:07 -0600

    public links exist now

--- DRAPI-Mod-Manager/main.cpp
+++ DRAPI-Mod-Manager/main.cpp
@@ -33,8 +33,8 @@ enum StateType {
 Color color_bg(15, 10, 25, 255), color_white(255, 255, 255, 255), color_selected(0, 255, 0, 255), color_deselected(128, 128, 128, 255);
 StateType curState = None;
 
-string launcherdataURL = "https://cdn.discordapp.com/attachments/849292573230104576/1061398094484353107/launcher_latest.json";
-string announcmentdataURL = "https://cdn.discordapp.com/attachments/849292573230104576/1060820994429812736/announcement.json";
+string launcherdataURL = "https://raw.githubusercontent.com/DillyzThe1/DRAPI-Mod-Manager/main/data/launcher_latest.json";
+string announcmentdataURL = "https://raw.githubusercontent.com/DillyzThe1/DRAPI-Mod-Manager/main/data/announcement.json";
 
 string appdatapath, aupath, aumoddedpath, launcherdatapath, announcmentsdatapath, bepinexzippath;
 json launcherjson, announcementjson;
